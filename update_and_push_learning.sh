a5648989b725efd1ff332410375976dba713a1d5
Neo-X <glen@fracturedplane.com>
2018-10-31 14:49:14 -0700

    Working on scripts

--- update_and_push_learning.sh
+++ update_and_push_learning.sh
@@ -5,5 +5,5 @@ sudo singularity shell --writable ubuntu_learning -c "cd /opt/TerrainRL; git pul
 ### compile image into simg and send out.
 sudo singularity build ubuntu_learning.simg ubuntu_learning
 
-scp ./ubuntu_learning.simg_back cedar.computecanada.ca:~/scratch/playground/singularity
+scp ./ubuntu_learning.simg cedar.computecanada.ca:~/scratch/playground/singularity
 
