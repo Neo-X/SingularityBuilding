ef3245b3d1b137417945b0eef5da15a35988fc5b
Neo-X <glen@fracturedplane.com>
2019-02-15 15:15:33 -0800

    Adding another script for compiling and not puching

---
+++ update_learning.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+### run command in container to update simulation library
+# sudo singularity shell --writable ubuntu_learning -c "pushd /opt/TerrainRL; git pull origin master; pushd simAdapter/; ./gen_swig.sh; popd; premake4 gmake; pushd gmake; make config=release64 -j 8; popd"
+#
+sudo singularity shell --writable /scratch/gberseth/playground/singularity/ubuntu_learning.img -c "pushd /opt/TerrainRL; git pull origin master; pushd simAdapter/; ./gen_swig.sh; popd; premake4 gmake; pushd gmake; make config=release64 -j 8; popd; popd; pushd RLSimulationEnvironments; git pull origin master; pushd rendering; premake4 gmake; make config=release64 -j 2; popd; popd;"
+
+
+### compile image into simg and send out.
+# sudo singularity build ubuntu_learning.simg ubuntu_learning
+
+# scp ./ubuntu_learning.img cedar.computecanada.ca:~/scratch/playground/singularity
+
