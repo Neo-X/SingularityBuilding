21ccda60765e3cc267a679667e82971af0ecfc48
Neo-X <glen@Fracturedplane.com>
2019-09-12 19:20:45 -0400

    Fixing commands

--- update_learning.sh
+++ update_learning.sh
@@ -2,7 +2,7 @@
 ### run command in container to update simulation library
 # sudo singularity shell --writable ubuntu_learning -c "pushd /opt/TerrainRL; git pull origin master; pushd simAdapter/; ./gen_swig.sh; popd; premake4 gmake; pushd gmake; make config=release64 -j 8; popd"
 #
-sudo singularity shell --writable /scratch/gberseth/playground/singularity/ubuntu_learning.img -c "pushd /opt/TerrainRL; git pull origin master; pushd simAdapter/; ./gen_swig.sh; popd; premake4 gmake; pushd gmake; make config=release64 -j 8; popd; popd; pushd RLSimulationEnvironments; git pull origin master; pushd rendering; premake4 gmake; make config=release64 -j 2; popd; popd;"
+sudo singularity shell --writable ./ubuntu_learning.img -c "pushd /opt/TerrainRLSim; git pull origin master; pushd simAdapter/; ./gen_swig.sh; popd; premake4 gmake; pushd gmake; make config=release64 -j 8; popd; popd; pushd /opt/RLSimulationEnvironments; git pull origin master; pushd rendering; premake4 gmake; make config=release64 -j 2; popd; popd;"
 
 
 ### compile image into simg and send out.
